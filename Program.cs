310284055fdae00bb1e8fc7a7252696a0ca13114
carlos eduardo <carloseduardo_alencar@outlook.com>
2021-03-13 12:07:13 -0300

    Término do programa Cebolinha

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("|Digite uma frase|");
+            Console.WriteLine("------------------");
+
+            Console.WriteLine();
+            string textoDigitado = Console.ReadLine();
+            string RecebeTexto = textoDigitado.Replace("r", "l").Replace("R", "L");
+            Console.WriteLine(RecebeTexto);
+           
+
+           
+          
+            
+
+         
+         
+
+        }
+    }
+}
